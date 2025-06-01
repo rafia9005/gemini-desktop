5dde47c078bcf99f94db0d7828525a26d5e88c04
rafia9005 <rafia9005@gmail.com>
2025-06-01 12:53:45 +0700

    chore: init @tailwindcss/vite

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -15,6 +16,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
   }
 })
